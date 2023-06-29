10c525f26f6d7b7e931874bf8e0422db5fbe334f
daniel_caldeira <76625872+Danniel-bc@users.noreply.github.com>
2023-06-29 15:12:13 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,28 @@
+// 1º)
+var numero1 = new Number(300);
+var numero2 = new Number(999);
+var numero3 = new Number(21);
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+// 2º)
+var number1 = Number.EPSILON;
+var number2 = Number.MAX_SAFE_INTEGER;
+var number3 = Number.MAX_VALUE;
+var number4 = Number.MIN_VALUE;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+
+// 3º)
+var numero = 150;
+
+console.log(numero);
+
+var string = numero.toString();
+
+console.log(string);
